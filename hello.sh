94bcd46e13e9cfaded318d2e413a2016274e548b
Hecea <81994410+Hecea@users.noreply.github.com>
2022-08-16 00:03:56 +0200

    Add files via upload
    single comment line

--- hello.sh
+++ hello.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# simple example for training
+# comment
+# is used to add single line of comment
+
 echo " Hello"
 echo "Print text  with  a new line"
-echo -n "Print tet without newline"
+echo -n "Print textt without newline"
 echo -e "\nRemoving \t backslash \t characters\n"
+
 
