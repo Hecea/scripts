1cd9f97003befb58d10886480f930b121bba57b6
Hecea <81994410+Hecea@users.noreply.github.com>
2022-08-31 23:48:21 +0200

    Add files via upload
    uname -m

--- hello.sh
+++ hello.sh
@@ -9,6 +9,9 @@ echo " Hello"
 echo "Print text  with  a new line"
 echo -n "Print text without newline"
 echo -e "\nRemoving \t backslash \t characters\n"
+logname
+uname -m
+
 
                                                                                                                                                                                                                                                                                                                                                                                                 
 
